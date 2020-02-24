1fab3c5d3d10d5c8a185ac8d23240d5daedce66a
Randy Taylor <tehgecKozzz@gmail.com>
2020-02-23 23:08:44 -0500

    Add utility file and a string split function

---
+++ src/util.hpp
@@ -0,0 +1,14 @@
+#include <sstream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> split(const std::string& input, const char delimiter) {
+    std::vector<std::string> tokens;
+    std::istringstream s(input);
+
+    for (std::string token; std::getline(s, token, delimiter);) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
