a938bff97c32191e37d2847c64e8e13da8d33190
Randy Taylor <tehgecKozzz@gmail.com>
2020-03-24 13:48:28 -0400

    Refactor test to support background

--- test/doctest/native/scenarios/register_in_course.cpp
+++ test/doctest/native/scenarios/register_in_course.cpp
@@ -13,81 +13,95 @@
 constexpr std::string_view host = "localhost";
 constexpr std::string_view port = "5001";
 
-TEST_SUITE("Registering in a course") {
-    University university;
+asio::io_context clerk_io_context;
+asio::io_context student_io_context;
+asio::io_context server_io_context;
 
-    asio::io_context clerk_io_context;
-    asio::io_context student_io_context;
-    tcp::resolver clerk_resolver{clerk_io_context};
-    tcp::resolver student_resolver{student_io_context};
-    const auto clerk_endpoints   = clerk_resolver.resolve(host, port);
-    const auto student_endpoints = student_resolver.resolve(host, port);
+std::thread server_thread;
+std::thread clerk_thread;
+std::thread student_thread;
 
-    asio::io_context server_io_context;
-    tcp::endpoint server_endpoint(tcp::v4(), 5001);
+tcp::resolver clerk_resolver{clerk_io_context};
+tcp::resolver student_resolver{student_io_context};
+const auto clerk_endpoints   = clerk_resolver.resolve(host, port);
+const auto student_endpoints = student_resolver.resolve(host, port);
+tcp::endpoint server_endpoint(tcp::v4(), 5001);
 
-    Client clerk{clerk_io_context, clerk_endpoints};
-    Client student{student_io_context, student_endpoints};
-    Server server{server_io_context, server_endpoint, university};
+University university;
 
-    std::thread server_thread;
-    std::thread clerk_thread;
-    std::thread student_thread;
+Client clerk{clerk_io_context, clerk_endpoints};
+Client student{student_io_context, student_endpoints};
+Server server{server_io_context, server_endpoint, university};
 
-    SCENARIO("Background") {
-        GIVEN("The server is running") {
-            server_thread  = std::thread([&] { server_io_context.run(); });
-            clerk_thread   = std::thread([&] { clerk_io_context.run(); });
-            student_thread = std::thread([&] { student_io_context.run(); });
-        }
+static void background() {
+    GIVEN("The server is running") {
+        server_thread  = std::thread([] { server_io_context.run(); });
+        clerk_thread   = std::thread([] { clerk_io_context.run(); });
+        student_thread = std::thread([] { student_io_context.run(); });
 
-        GIVEN("The clerk is logged in") { the_clerk_is_logged_in(clerk); }
+        GIVEN("The clerk is logged in") {
+            the_clerk_is_logged_in(clerk);
 
-        GIVEN("There is an existing course 12345, Witchcraft, 1") {
-            there_is_an_existing_course(clerk, "12345, Witchcraft, 1");
-        }
+            GIVEN("There is an existing course 12345, Witchcraft, 1") {
+                there_is_an_existing_course(clerk, "12345, Witchcraft, 1");
 
-        GIVEN("There is an existing student 123456789, joe") {
-            there_is_an_existing_student(clerk, "123456789, joe");
-        }
+                GIVEN("There is an existing student 123456789, joe") {
+                    there_is_an_existing_student(clerk, "123456789, joe");
 
-        GIVEN("There is an existing student 123456788, murphy") {
-            there_is_an_existing_student(clerk, "123456789, joe");
-        }
+                    GIVEN("There is an existing student 123456788, murphy") {
+                        there_is_an_existing_student(clerk,
+                                                     "123456788, murphy");
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                        std::this_thread::sleep_for(
+                            std::chrono::milliseconds(10));
 
-        GIVEN("The student has logged in as 123456789, joe") {
-            the_student_has_logged_in_as(student, "123456789, joe");
-        }
-    }
-
-    SCENARIO("A student registers in a course of the current term") {
-        GIVEN("We fastforward 10 days.") {
-            std::this_thread::sleep_for(std::chrono::seconds(DAY_LENGTH * 10));
-        }
-
-        GIVEN("The student enters register for course") {
-            student.send(Message("rfc"));
-        }
-
-        WHEN("The student enters 12345") { student.send(Message("12345")); }
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-
-        THEN("The student 123456789 is registered in the course 12345") {
-            CHECK(true ==
-                  university.course(12345).value().has_student(123456789));
-        }
-
-        AND_THEN("Death time") {
-            clerk_io_context.stop();
-            student_io_context.stop();
-            server_io_context.stop();
-
-            clerk_thread.join();
-            student_thread.join();
-            server_thread.join();
+                        GIVEN("The student has logged in as 123456789, joe") {
+                            the_student_has_logged_in_as(student,
+                                                         "123456789, joe");
+                        }
+                    }
+                }
+            }
         }
     }
 }
+
+static void teardown() {
+    clerk_io_context.stop();
+    student_io_context.stop();
+    server_io_context.stop();
+
+    clerk_thread.join();
+    student_thread.join();
+    server_thread.join();
+}
+
+TEST_SUITE_BEGIN("Registering in a course");
+
+SCENARIO("A student registers in a course of the current term") {
+    background();
+
+    GIVEN("We fastforward 10 days.") {
+        std::this_thread::sleep_for(std::chrono::seconds(DAY_LENGTH * 10));
+
+        GIVEN("The student enters register for course") {
+            student.send(Message("rfc"));
+
+            WHEN("The student enters 12345") {
+                student.send(Message("12345"));
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+                THEN(
+                    "The student 123456789 is registered in the course 12345") {
+                    CHECK(true == university.course(12345).value().has_student(
+                                      123456789));
+                }
+
+                teardown();
+            }
+        }
+    }
+}
+
+TEST_SUITE_END();
