d3818e8a19e9b5f0719b69fec8d9eb68b9516f06
Randy Taylor <tehgecKozzz@gmail.com>
2020-04-09 21:38:59 -0400

    Remove commented out code

--- src/networking/server/server.hpp
+++ src/networking/server/server.hpp
@@ -8,7 +8,6 @@ using asio::ip::tcp;
 
 class Server {
 public:
-    // Server(asio::io_context&, const tcp::endpoint&);
     Server(asio::io_context&, const tcp::endpoint&, University&);
 
 private:
